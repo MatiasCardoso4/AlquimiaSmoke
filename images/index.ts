56572b4dacc3cb941d2eddd0587cb312262402b9
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-03-25 20:32:24 -0300

    Updates

---
+++ images/index.ts
@@ -0,0 +1,4 @@
+export { default as logo } from './logo.png';
+export { default as lupa } from './lupa.png';
+export { default as pizzaCategoryImage } from './pizza-category.png';
+export { default as hamburgerCategoryImage } from './hamburger-category.png';
---
+++ products/hamburgers_images/index.ts
@@ -0,0 +1,7 @@
+export { default as Avocado_burguer } from "./Avocado_Burger.png";
+export { default as BBQ_burger } from "./BBQ_Burger.png";
+export { default as Cheeseburger } from "./Cheeseburger.png";
+export { default as Chicken_burger } from "./Chicken_Burger.png";
+export { default as Double_burger } from "./Double_burguer.png";
+export { default as Mushroom_swiss } from "./Mushroom_Swiss.png";
+export { default as Spicy_burger } from "./Spicy_Burger.png";
