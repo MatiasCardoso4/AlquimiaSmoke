5ab2f2e6b9f983c73d14ab28af6e104ad94e905b
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-03-24 21:16:08 -0300

    Update image paths and favicon link; add new images for categories

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import { CartHeaderTag } from "../CartHeaderTag/CartHeaderTag";
 export const Header = () => {
   return (
     <header className=" flex w-full justify-between p-2 lg:p-4 ">
-      <Link to={'/'}>
+      <Link to={'/AlquimiaSmoke'}>
         <img
-          src={"../../../public/logo.png"}
+          src={"../../../public/images/logo.png"}
           alt="logo image"
           className="w-20 md:w-32 lg:w-48 "
         />
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
 import { ProductProvider } from "./context/ProductsContext/ProductProvider.tsx";
 import { CartProvider } from "./context/CartContext/CartProvider.tsx";
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
